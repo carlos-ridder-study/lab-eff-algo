58d7b6eeed61e17dee86af7a3f88274f56a4f917
Ruben Kemna <ruben-kemna@web.de>
2025-05-02 16:01:27 +0200

    rect fields bit ops success submission

--- exercise_2/java/RectFields.java
+++ exercise_2/java/RectFields.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.BitSet;
 
 public class RectFields {
     public static void main(String[] args) throws IOException {
@@ -8,32 +9,36 @@ public class RectFields {
 
         int rows = Integer.parseInt(reader.readLine().trim());
 
-        //row wise strings into chars arrays
+        // row wise strings into chars arrays
         String[] grid = new String[rows];
         for (int i = 0; i < rows; i++) {
             grid[i] = reader.readLine().trim();
         }
 
-        int columns = grid[0].length();  
-        int[][] binary = new int[rows][columns];
+        int columns = grid[0].length();
+
+        //bitSet instead of int for bitwise operations
+        BitSet[] binary = new BitSet[rows];
         for (int i = 0; i < rows; i++) {
+            binary[i] = new BitSet(columns);
             char[] charRow = grid[i].toCharArray();
             for (int j = 0; j < columns; j++) {
-                binary[i][j] = charRow[j] - '0'; //ascii subtract to make it either 1 or 0
+                if (charRow[j] == '1') {
+                    binary[i].set(j);
+                }
             }
         }
 
-        //dont know a smarter way than to quadratically iterate row pairs
+        // don't know a smarter way than to quadratically iterate row pairs
         long count = 0;
         for (int row1 = 0; row1 < rows; row1++) {
             for (int row2 = row1 + 1; row2 < rows; row2++) {
-                int verticals = 0;
-                for (int col = 0; col < columns; col++) {
-                    if (binary[row1][col] == 1 && binary[row2][col] == 1) {
-                        verticals++;
-                    }
-                }
-                // with all column aligned pairs, we an calc the combinations with verticals choose 2 without permutation
+                
+                BitSet intersection = (BitSet) binary[row1].clone();
+                intersection.and(binary[row2]);
+                int verticals = intersection.cardinality();
+
+                // with all column aligned pairs, we can calc the combinations with verticals choose 2 without permutation
                 count += (long) verticals * (verticals - 1) / 2;
             }
         }
