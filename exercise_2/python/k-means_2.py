03ad42ed4abb9dd0f103f9d717d90971f1eab734
carlos-ridder-study <carlos.ridder@uni-bonn.de>
2025-05-06 09:39:21 +0200

    try cover tree approach

--- exercise_2/python/k-means_2.py
+++ exercise_2/python/k-means_2.py
@@ -31,6 +31,51 @@ class Vector:
     
     def __str__(self):
         return " ".join(str(x) for x in self.coords)
+    
+class CoverTree:
+    def __init__(self, point, level):
+        self.point = point
+        self.level = level
+        self.children = []
+        
+    def insert(self, point, level):
+        # Check if this node can cover the point at current level
+        if (self.point - point).abs() > 2**level:
+            return False
+
+        for child in self.children:
+            if child.insert(point, level - 1):
+                return True
+
+        # Insert as new child
+        new_child = CoverTree(point, level - 1)
+        self.children.append(new_child)
+        return True
+
+    def insert_point(self, point):
+        level = self.level
+        while not self.insert(point, level):
+            # Promote root
+            new_root = CoverTree(self.point, level + 1)
+            new_root.children.append(self)
+            old_point, old_children, old_level = self.point, self.children, self.level
+            self.point = old_point
+            self.level += 1
+            self.children = [CoverTree(old_point, old_level)] + old_children
+            level += 1
+        
+    def is_close(self, point, radius):
+        # Check if a point is close to the cover tree node
+        dist = (self.point - point).abs()
+        if dist <= radius:
+            return True
+        for child in self.children:
+            # use triangle inequality as lower bound of distance from point to child
+            lower_bound = (child.point - point).abs() - 2**child.level
+            if lower_bound <= radius:
+                # children are close enough to check
+                return child.is_close(point, radius)
+        return False
 
 line = sys.stdin.readline().strip().split(" ")
 d = int(line[0])
@@ -38,21 +83,36 @@ R = float(line[1])
 
 line = sys.stdin.readline().strip()
 centers = []
-R = R**2
+R2 = R**2
 
+"""
+full linear search
 while line != "":
-    line = line.split(" ")
-    coords = []
-    for i in range(d):
-        coords.append(float(line[i]))
-    point = Vector(coords)
-    dist = R + 1
+    point = Vector([float(x) for x in line.split(" ")])
+    dist = R2 + 1
     i = 0
-    while dist > R and i < len(centers):
+    while dist > R2 and i < len(centers):
         dist = (point - centers[i]).squarednorm()
         i += 1
-    if dist > R:
+    if dist > R2:
         centers.append(point)
     line = sys.stdin.readline().strip()
         
 print(len(centers))
+"""
+center_count = 0
+if line != "":
+    point = Vector([float(x) for x in line.split(" ")])
+    center_count += 1
+    cover_radius = math.ceil(math.log2(R))
+    centers = CoverTree(point, cover_radius + 10)
+    line = sys.stdin.readline().strip()
+
+while line != "":
+    point = Vector([float(x) for x in line.split(" ")])
+    if not centers.is_close(point, R):
+        center_count += 1
+        centers.insert_point(point)
+    line = sys.stdin.readline().strip()
+    
+print(center_count)
